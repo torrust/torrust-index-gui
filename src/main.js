97e0a41af97d037401b44e0848b968427221c7d3
Mick <mickvd99@gmail.com>
2021-08-06 16:17:14 +0200

    refactor: added <router-view> to App.vue

--- src/main.js
+++ src/main.js
@@ -1,8 +1,27 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+// import Layout from "@/components/Layout";
+// import Home from "@/views/Home";
+
+Vue.use(VueRouter)
 
 Vue.config.productionTip = false
 
+const routes = [
+  { path: '/', redirect: { name: 'Home' } },
+  // { path: '/', component: Layout, children: [
+  //     { path: 'home', name: 'Home', component: Home }
+  //   ]
+  // }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  routes
+})
+
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
