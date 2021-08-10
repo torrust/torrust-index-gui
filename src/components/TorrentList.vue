e005e3e7ef4c37157d38321189d69417ee5a721e
Mick <mickvd99@gmail.com>
2021-08-10 13:06:27 +0200

    style: added colored border on hover to TorrentList

--- src/components/TorrentList.vue
+++ src/components/TorrentList.vue
@@ -14,26 +14,26 @@
         </thead>
         <tbody>
           <tr class="text-center cursor-pointer duration-100 hover:bg-blue-100" v-for="i in 16" :key="i">
-            <td class="p-3 border-2 border-r-0 border-gray-200">
+            <td class="p-3">
               <div class="flex flex-row items-center">
                 <img class="rounded-lg h-12 w-12  object-cover" src="https://i2.extraimage.info/pix/2021/08/08/712335bc3cf031c553b4f15bb3a79ad9.png" alt="unsplash image">
                 <div class="ml-3 text-black font-bold">Borderlands 3: Director's Cut [WB]</div>
               </div>
             </td>
-            <td class="p-3 border-t-2 border-b-2 border-gray-200">
+            <td class="p-3">
               <span class="text-green-400 font-bold">1787</span>
             </td>
-            <td class="p-3 border-t-2 border-b-2 border-gray-200">
+            <td class="p-3">
               <span class="text-red-400 font-bold">2376</span>
             </td>
-            <td class="p-3 border-t-2 border-b-2 border-gray-200">
+            <td class="p-3">
               Today
             </td>
-            <td class="p-3 border-t-2 border-b-2 border-gray-200">
+            <td class="p-3">
               <span class="text-black">674 MB</span>
             </td>
-            <td class="p-3 border-2 border-l-0 border-gray-200">
-              <span class="text-blue-400 font-semibold">Warm Beer</span>
+            <td class="p-3">
+              <span class="text-blue-400 font-bold">Warm Beer</span>
             </td>
           </tr>
         </tbody>
@@ -48,7 +48,7 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 .table {
   border-spacing: 0 15px;
 }
@@ -61,13 +61,26 @@ i {
   border-radius: 20px;
 }
 
-tr td:nth-child(n+6),
-tr th:nth-child(n+6) {
-  border-radius: 0 .625rem .625rem 0;
+tr {
+  &:hover {
+    td {
+      @apply duration-100;
+      @apply border-blue-400;
+    }
+  }
 }
 
-tr td:nth-child(1),
-tr th:nth-child(1) {
+tr td {
+  @apply border-t-2 border-b-2 border-gray-200;
+}
+
+tr td:nth-child(n+6) {
+  border-radius: 0 .625rem .625rem 0;
+  @apply border-r-2;
+}
+
+tr td:nth-child(1) {
   border-radius: .625rem 0 0 .625rem;
+  @apply border-l-2;
 }
 </style>
