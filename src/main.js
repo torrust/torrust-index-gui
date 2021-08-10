7f242bb56e8cf2bc273507e2f34bb475bbae72ca
Mick <mickvd99@gmail.com>
2021-08-10 14:44:19 +0200

    refactor: added timeSince and fileSize global functions

--- src/main.js
+++ src/main.js
@@ -11,6 +11,45 @@ Vue.use(VueRouter)
 
 Vue.config.productionTip = false
 
+Vue.mixin({
+  methods: {
+    timeSince(date) {
+      const seconds = Math.floor((+new Date() - date) / 1000);
+      let interval = Math.floor(seconds / 31536000);
+      if (interval >= 1) {
+        return `${interval} year${(interval > 1 ? 's' : '')}`;
+      }
+      interval = Math.floor(seconds / 2592000);
+      if (interval >= 1) {
+        return `${interval} month${(interval > 1 ? 's' : '')}`;
+      }
+      interval = Math.floor(seconds / 86400);
+      if (interval >= 1) {
+        return `${interval} day${(interval > 1 ? 's' : '')}`;
+      }
+      interval = Math.floor(seconds / 3600);
+      if (interval >= 1) {
+        return `${interval} hour${(interval > 1 ? 's' : '')}`;
+      }
+      interval = Math.floor(seconds / 60);
+      if (interval >= 1) {
+        return `${interval} minute${(interval > 1 ? 's' : '')}`;
+      }
+      return `${Math.floor(seconds)} seconds`;
+    },
+    // get string of file size in bytes
+    fileSize(size) {
+      let sizeString = `${(size).toFixed(2)} B`;
+
+      if (size / 1000000000 < 1000) sizeString = `${(size / 1000000000).toFixed(2)} GB`;
+      if (size / 1000000 < 1000) sizeString = `${(size / 1000000).toFixed(2)} MB`;
+      if (size / 1000 < 1000) sizeString = `${(size / 1000).toFixed(2)} KB`;
+
+      return sizeString;
+    },
+  },
+})
+
 const routes = [
   { path: '/', redirect: { name: 'Home' } },
   { path: '/', component: Layout, children: [
