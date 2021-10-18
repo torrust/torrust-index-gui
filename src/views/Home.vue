27b13bbdb6c50b283de3afcce513bc07ef35f840
Mick <mickvd99@gmail.com>
2021-10-18 20:40:30 +0200

    feat: add home (popular) page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,19 +2,9 @@
   <div class="bg-primary p-6 rounded-3xl">
     <div class="flex justify-between">
       <h1 class="view-title text-white">Most Popular</h1>
-      <TableOrder v-if="torrents.results.length > 0" :sorting.sync="sorting"/>
     </div>
 
-    <!--    <transition-->
-    <!--        enter-active-class="transition ease-out duration-100 transform"-->
-    <!--        enter-class="opacity-0 scale-95"-->
-    <!--        enter-to-class="opacity-100 scale-100"-->
-    <!--        leave-active-class="transition ease-in duration-75 transform"-->
-    <!--        leave-class="opacity-100 scale-100"-->
-    <!--        leave-to-class="opacity-0 scale-95"-->
-    <!--    >-->
     <router-view/>
-    <!--    </transition>-->
 
     <TorrentList v-if="torrents.results.length > 0" :torrents="torrents.results" />
     <Pagination v-if="torrents.results.length > 0" :current-page.sync="currentPage" :total-pages="totalPages" :total-results="torrents.total" :page-size="pageSize" />
@@ -24,15 +14,14 @@
 
 <script>
 import TorrentList from "@/components/TorrentList";
-import TableOrder from "@/components/TableOrder";
 import Pagination from "@/components/Pagination";
 import HttpService from "@/common/http-service";
 
 export default {
   name: 'Home',
-  components: {TableOrder, Pagination, TorrentList},
+  components: {Pagination, TorrentList},
   data: () => ({
-    sorting: "uploaded",
+    category: 'popular',
     torrents: {
       total: 0,
       results: []
@@ -41,35 +30,25 @@ export default {
     pageSize: 20,
   }),
   methods: {
-    loadTorrents(category, page, sort) {
-      HttpService.get(`/category/${category}/torrents?page_size=${this.pageSize}&page=${page-1}&sort=${sort}`, (res) => {
+    loadTorrents(category, page) {
+      HttpService.get(`/category/${category}/torrents?page_size=${this.pageSize}&page=${page-1}`, (res) => {
         this.torrents = res.data.data;
       }).catch(() => {
       });
     }
   },
   computed: {
-    category() {
-      return this.$route.params?.name;
-    },
     totalPages() {
       return Math.ceil(this.torrents.total / this.pageSize);
     },
   },
   watch: {
-    category(newCat) {
-      this.loadTorrents(newCat, this.currentPage, this.sorting);
-    },
-    sorting(newSort) {
-      this.loadTorrents(this.category, this.currentPage, newSort);
-    },
     currentPage(newPage) {
-      this.loadTorrents(this.category, newPage, this.sorting);
+      this.loadTorrents(this.category, newPage);
     }
   },
   mounted() {
-    let category = 'popular';
-    this.loadTorrents(category, this.currentPage, this.sorting);
+    this.loadTorrents(this.category, this.currentPage);
   }
 }
 </script>
