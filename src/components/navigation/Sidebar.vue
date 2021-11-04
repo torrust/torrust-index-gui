ca43ad3d9d557065fab5f347c67493da4a6ea856
Warm Beer <mickvd99@gmail.com>
2021-11-04 01:04:22 +0100

    refactor: updated routing in sidebar

--- src/components/navigation/Sidebar.vue
+++ src/components/navigation/Sidebar.vue
@@ -15,20 +15,20 @@
     </div>
 
     <div class="mt-6 px-4 text-gray-300">
-      <router-link :to="'/categories'" class="mt-2 w-full flex justify-center items-center h-10 rounded-lg cursor-pointer" exact-active-class="text-white bg-secondary">
-        <span>Categories</span>
-      </router-link>
-      <router-link :to="`/categories/popular`" class="mt-2 w-full flex items-center h-10 pl-4 rounded-lg cursor-pointer" exact-active-class="text-white bg-secondary">
+      <router-link :to="`/torrents/popular`" class="mt-2 w-full flex items-center h-10 pl-4 rounded-lg cursor-pointer" exact-active-class="text-white bg-secondary">
         <span>Most Popular</span>
       </router-link>
-      <router-link :to="`/categories/recent`" class="mt-2 w-full flex items-center h-10 pl-4 rounded-lg cursor-pointer" exact-active-class="text-white bg-secondary">
+      <router-link :to="`/torrents/recent`" class="mt-2 w-full flex items-center h-10 pl-4 rounded-lg cursor-pointer" exact-active-class="text-white bg-secondary">
         <span>Most Recent</span>
       </router-link>
-      <template v-for="(category, index) in categories">
-        <router-link :key="index" :to="`/categories/${urlSafe(category.name)}`" class="mt-2 w-full flex items-center h-10 pl-4 rounded-lg cursor-pointer" exact-active-class="text-white bg-secondary">
-          <span>{{ titleCase(category.name) }} ({{ category.num_torrents }})</span>
-        </router-link>
-      </template>
+      <router-link :to="'/categories'" class="mt-2 w-full flex items-center h-10 pl-4 rounded-lg cursor-pointer" exact-active-class="text-white bg-secondary">
+        <span>Browse Categories</span>
+      </router-link>
+<!--      <template v-for="(category, index) in categories">-->
+<!--        <router-link :key="index" :to="`/categories/${urlSafe(category.name)}`" class="mt-2 w-full flex items-center h-10 pl-4 rounded-lg cursor-pointer" exact-active-class="text-white bg-secondary">-->
+<!--          <span>{{ titleCase(category.name) }} ({{ category.num_torrents }})</span>-->
+<!--        </router-link>-->
+<!--      </template>-->
     </div>
   </div>
 </template>
