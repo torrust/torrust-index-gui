ecd5aa7757831e562916addd135fe0dd40fb782c
Octavian <50994966+BelieveInBunny@users.noreply.github.com>
2022-06-09 17:11:02 +0530

    small spelling changes

--- src/views/Pages.vue
+++ src/views/Pages.vue
@@ -33,13 +33,13 @@
         <MarkdownItVue v-if="!editingDescription && page.description" :content="page.description" class="md-body max-w-none prose-sm prose-blue" />
         <span v-if="!editingDescription && !page.description" class="text-slate-400 italic">Empty</span>
     </div>
-    <h1 v-else-if="!loading" class="py-6 text-slate-600 italic">Torrent not found.</h1>
+    <h1 v-else-if="!loading" class="py-6 text-slate-600 italic">Page not found.</h1>
     <div v-else class="flex flex-row text-slate-400 items-center" disabled>
       <svg class="animate-spin h-5 w-5 mr-3 text-sky-500" viewBox="0 0 24 24">
         <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
         <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
       </svg>
-      Loading torrent information...
+      Loading page information...
     </div>
   </div>
 </template>
