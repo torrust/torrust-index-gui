be940c22dac016f460c333e3af9b13f56ee76895
Warm Beer <mickvd99@gmail.com>
2022-09-17 12:32:42 +0200

    feat: added Tabs.vue component

---
+++ src/components/navigation/Tabs.vue
@@ -0,0 +1,33 @@
+<template>
+  <ul class="flex flex-wrap border-b-2 border-slate-600">
+    <li v-for="(tab) in tabs">
+      <router-link
+          :to="tab.path"
+          class="inline-block py-2 px-4 font-medium text-center text-slate-400 hover:text-slate-200 border-b-2 border-transparent duration-200"
+          style="margin-bottom: -2px;"
+          :class="{ 'active': $route.path === tab.path }"
+      >
+        {{ tab.name }}
+      </router-link>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "Tabs",
+  props: {
+    tabs: {
+      type: Array,
+      required: true,
+      default: () => [{name: "example1", path: "/settings"}, {name: "example2", path: "/inactive"}],
+    },
+  }
+}
+</script>
+
+<style scoped>
+.active {
+  @apply text-slate-200 border-slate-200;
+}
+</style>
