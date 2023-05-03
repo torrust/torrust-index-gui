219cff2e9e2522c9baf120ad98b6a8d729926a0f
Warm Beer <mickvd99@gmail.com>
2023-05-03 19:29:20 +0200

    style: fix `TorrustButton` color

--- components/TorrustButton.vue
+++ components/TorrustButton.vue
@@ -1,6 +1,6 @@
 <template>
   <button
-    class="px-6 h-12 w-full inline-flex justify-center items-center appearance-none bg-primary-dark hover:bg-primary font-semibold text-sm text-neutral-content capitalize rounded-2xl cursor-pointer duration-500 disabled:bg-base-300 disabled:text-neutral-content/50"
+    class="px-6 h-12 btn btn-primary w-full inline-flex justify-center items-center appearance-none capitalize rounded-2xl cursor-pointer duration-500 disabled:bg-neutral-600 disabled:text-neutral-content/50"
     :label="label"
   >
     {{ label }}
