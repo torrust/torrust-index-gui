8c853aa81c41d19370b94b53439297dce2fdb132
Jose Celano <josecelano@gmail.com>
2023-06-26 14:07:35 +0100

    feat!: read-only backend settings
    Backend settings are no longer updatable from the frontend app. We have
    to split settings into two types:
    
    - Configuration innjected only when the service starts (config.toml file
      or env vars).
    - Settings that can be dinamycally changed by the website admin whitout
      restarting the service, like categories and tags.

--- components/navigation/NavigationBar.vue
+++ components/navigation/NavigationBar.vue
@@ -1,32 +1,32 @@
 <template>
-  <div class="px-4 md:pb-0 md:px-8 lg:px-24 flex flex-col bg-base-200">
-    <div class="p-0 navbar gap-2">
+  <div class="flex flex-col px-4 md:pb-0 md:px-8 lg:px-24 bg-base-200">
+    <div class="gap-2 p-0 navbar">
       <button class="flex md:hidden btn btn-square btn-ghost" @click="mobileCollapsed = !mobileCollapsed">
         <Bars3Icon class="w-8" />
       </button>
-      <NuxtLink to="/" class="flex flex-row btn btn-ghost normal-case text-xl">
+      <NuxtLink to="/" class="flex flex-row text-xl normal-case btn btn-ghost">
         <span>{{ settings?.website_name || "Torrust" }}</span>
       </NuxtLink>
-      <div class="form-control hidden md:flex">
+      <div class="hidden form-control md:flex">
         <input
           v-model="searchQuery"
           name="search"
           type="text"
-          class="input border-2 input-bordered rounded-2xl placeholder-neutral-content"
+          class="border-2 input input-bordered rounded-2xl placeholder-neutral-content"
           :placeholder="`Search ${settings?.website_name ?? 'Torrust'}`"
           @keyup.enter="submitSearch"
           @focusin="typingInSearch = true"
           @focusout="typingInSearch = false"
         >
       </div>
-      <div class="px-2 dropdown dropdown-hover hidden md:inline-block text-base-content/75 hover:text-base-content">
+      <div class="hidden px-2 dropdown dropdown-hover md:inline-block text-base-content/75 hover:text-base-content">
         <label tabindex="0" class="px-1">
           <NuxtLink to="/torrents" class="flex">
             Torrents
-            <ChevronDownIcon class="ml-2 w-5" />
+            <ChevronDownIcon class="w-5 ml-2" />
           </NuxtLink>
         </label>
-        <ul tabindex="0" class="p-2 shadow menu dropdown-content bg-base-100 rounded w-52">
+        <ul tabindex="0" class="p-2 rounded shadow menu dropdown-content bg-base-100 w-52">
           <li><NuxtLink to="/torrents?sorting=LeechersDesc">
             Most Popular
           </NuxtLink></li>
@@ -35,7 +35,7 @@
           </NuxtLink></li>
         </ul>
       </div>
-      <div class="ml-auto flex-none gap-2">
+      <div class="flex-none gap-2 ml-auto">
         <template v-if="user">
           <NuxtLink to="/upload" class="hidden md:flex btn bg-base-100 rounded-2xl">
             Upload Torrent
@@ -46,8 +46,8 @@
                 <UserCircleIcon />
               </div>
             </label>
-            <ul tabindex="0" class="mt-3 p-2 shadow menu dropdown-content bg-base-100 rounded-box w-52">
-              <li><NuxtLink to="/admin/settings/general">
+            <ul tabindex="0" class="p-2 mt-3 shadow menu dropdown-content bg-base-100 rounded-box w-52">
+              <li><NuxtLink to="/admin/settings/backend">
                 Admin Settings
               </NuxtLink></li>
               <li><a @click="logoutUser()">Logout</a></li>
@@ -62,14 +62,14 @@
       </div>
     </div>
     <template v-if="!mobileCollapsed">
-      <div class="py-4 md:hidden items-center justify-between w-full">
+      <div class="items-center justify-between w-full py-4 md:hidden">
         <div class="flex flex-row gap-2">
-          <div class="form-control w-full">
+          <div class="w-full form-control">
             <input
               v-model="searchQuery"
               name="search"
               type="text"
-              class="input input-bordered border-2"
+              class="border-2 input input-bordered"
               :placeholder="`Search ${settings?.website_name ?? 'Torrust'}`"
               @keyup.enter="submitSearch"
               @focusin="typingInSearch = true"
@@ -80,7 +80,7 @@
             <MagnifyingGlassIcon class="w-6" />
           </button>
         </div>
-        <ul id="mobile-menu" class="flex flex-col gap-2 p-4 mt-4 font-medium bg-base-content/10 rounded-lg">
+        <ul id="mobile-menu" class="flex flex-col gap-2 p-4 mt-4 font-medium rounded-lg bg-base-content/10">
           <li>
             <NuxtLink to="/" class="block py-2 pl-3 pr-4 text-white rounded-lg">
               Home
@@ -93,7 +93,7 @@
           </li>
         </ul>
         <template v-if="user">
-          <NuxtLink to="/upload" class="mt-4 btn btn-secondary w-full">
+          <NuxtLink to="/upload" class="w-full mt-4 btn btn-secondary">
             Upload Torrent
           </NuxtLink>
         </template>
--- components/navigation/NavigationBarBackup.vue
+++ components/navigation/NavigationBarBackup.vue
@@ -1,12 +1,12 @@
 <template>
   <div
-    class="flex flex-col sticky top-0 h-16 md:h-20 justify-center bg-base-100/80 z-40"
+    class="sticky top-0 z-40 flex flex-col justify-center h-16 md:h-20 bg-base-100/80"
     style="backdrop-filter: blur(20px);"
     @mouseleave="closeDropdowns()"
   >
     <div class="flex flex-col w-full">
       <!-- MOBILE -->
-      <div id="mobile-menu" class="flex md:hidden flex-row items-center max-w-full">
+      <div id="mobile-menu" class="flex flex-row items-center max-w-full md:hidden">
         <div v-if="mobileState === MobileState.Navigate" id="open-mobile-search-bar-toggle" class="px-3.5 h-10 mr-1 flex md:hidden flex-col justify-center items-center bg-white/5 border border-slate-600 rounded-2xl">
           <button class="flex flex-col" @click="mobileState = MobileState.Search">
             <MagnifyingGlassIcon size="18" class="text-slate-400" />
@@ -16,19 +16,19 @@
           <div class="flex flex-col">
             <span
               id="explore"
-              class="px-6 h-10 flex flex-row flex-nowrap items-center appearance-none text-lg text-slate-200 hover:text-white font-medium cursor-pointer duration-200"
+              class="flex flex-row items-center h-10 px-6 text-lg font-medium duration-200 appearance-none cursor-pointer flex-nowrap text-slate-200 hover:text-white"
               @mouseover="exploreDropdown = true"
               @mouseleave="exploreDropdown = false"
             >
               <span>Explore</span>
               <!--                <ChevronDownIcon size="18" class="ml-1"></ChevronDownIcon>-->
               <div
-                class="absolute -ml-4 pt-60 z-10"
+                class="absolute z-10 -ml-4 pt-60"
                 :class="{hidden: !exploreDropdown}"
               >
-                <div class="w-48 divide-y bg-base-200 rounded-2xl overflow-hidden drop-shadow" @click.prevent="exploreDropdown = false">
-                  <ul class="text-sm text-slate-400 font-medium duration-200" aria-labelledby="dropdownDefault">
-                    <li class="p-4 w-full hover:bg-slate-700 hover:text-white duration-200">
+                <div class="w-48 overflow-hidden divide-y bg-base-200 rounded-2xl drop-shadow" @click.prevent="exploreDropdown = false">
+                  <ul class="text-sm font-medium duration-200 text-slate-400" aria-labelledby="dropdownDefault">
+                    <li class="w-full p-4 duration-200 hover:bg-slate-700 hover:text-white">
                       <router-link to="/torrents" replace class="inline-flex items-center">
                         <span class="flex flex-nowrap whitespace-nowrap">Torrents</span>
                       </router-link>
@@ -39,8 +39,8 @@
             </span>
           </div>
         </div>
-        <div v-if="mobileState === MobileState.Navigate" id="extra-options" class="flex flex-row flex-1 ml-auto items-center justify-end">
-          <router-link to="/upload" class="px-4 h-10 inline-flex flex-nowrap justify-center items-center self-start appearance-none bg-sky-500 hover:bg-sky-600 text-sm text-white font-medium rounded-2xl cursor-pointer duration-200">
+        <div v-if="mobileState === MobileState.Navigate" id="extra-options" class="flex flex-row items-center justify-end flex-1 ml-auto">
+          <router-link to="/upload" class="inline-flex items-center self-start justify-center h-10 px-4 text-sm font-medium text-white duration-200 appearance-none cursor-pointer flex-nowrap bg-sky-500 hover:bg-sky-600 rounded-2xl">
             <ArrowUpTrayIcon size="24" />
           </router-link>
         </div>
@@ -54,7 +54,7 @@
                       v-model="searchQuery"
                       name="search"
                       type="text"
-                      class="h-12 bg-transparent outline-0 text-slate-200 text-sm font-medium placeholder-slate-400"
+                      class="h-12 text-sm font-medium bg-transparent outline-0 text-slate-200 placeholder-slate-400"
                       placeholder="Search by torrent, category or user"
                       @keyup.enter="submitSearch"
                     >
@@ -64,7 +64,7 @@
             </div>
           </div>
         </div>
-        <div v-if="mobileState === MobileState.Search" id="close-mobile-search-bar-toggle" class="px-4 h-12 flex md:hidden flex-col justify-center items-center bg-white/5 rounded-2xl">
+        <div v-if="mobileState === MobileState.Search" id="close-mobile-search-bar-toggle" class="flex flex-col items-center justify-center h-12 px-4 md:hidden bg-white/5 rounded-2xl">
           <button class="flex flex-col" @click="mobileState = MobileState.Navigate">
             <XMarkIcon size="18" class="text-slate-400" />
           </button>
@@ -72,15 +72,15 @@
       </div>
 
       <!-- DESKTOP -->
-      <div id="desktop-menu" class="hidden md:flex flex-row items-center">
-        <div id="left-menu" class="hidden md:flex flex-row justify-start">
-          <div id="site-name" class="hidden md:flex flex-row">
+      <div id="desktop-menu" class="flex-row items-center hidden md:flex">
+        <div id="left-menu" class="flex-row justify-start hidden md:flex">
+          <div id="site-name" class="flex-row hidden md:flex">
             <div class="flex flex-col">
-              <router-link class="block text-2xl text-neutral-content duration-200" to="/">
+              <router-link class="block text-2xl duration-200 text-neutral-content" to="/">
                 <div class="flex flex-row flex-nowrap">
                   <!-- Placeholder LOGO -->
-                  <div class="mr-3 w-8 h-8 flex flex-col items-center justify-center bg-gradient-to-bl from-primary to-primary-focus rounded-md shrink-0">
-                    <span class="font-bold text-2xl text-base-100 antialiased">T</span>
+                  <div class="flex flex-col items-center justify-center w-8 h-8 mr-3 rounded-md bg-gradient-to-bl from-primary to-primary-focus shrink-0">
+                    <span class="text-2xl antialiased font-bold text-base-100">T</span>
                   </div>
                   <span class="block w-full text-2xl font-semibold">{{ settings?.website_name || "Torrust" }}</span>
                 </div>
@@ -88,7 +88,7 @@
             </div>
           </div>
         </div>
-        <div id="search-bar" class="ml-6 hidden md:block mx-5 grow max-w-sm">
+        <div id="search-bar" class="hidden max-w-sm mx-5 ml-6 md:block grow">
           <div class="flex flex-col">
             <div class="flex flex-col">
               <div class="px-3.5 text-sm bg-base-200 ring-2 ring-base-content/25 hover:[&:not(:focus-within)]:ring-primary/50 focus-within:ring-primary rounded-xl duration-200">
@@ -98,14 +98,14 @@
                       v-model="searchQuery"
                       name="search"
                       type="text"
-                      class="h-10 bg-transparent outline-0 text-neutral-content font-medium placeholder-neutral-content/40"
+                      class="h-10 font-medium bg-transparent outline-0 text-neutral-content placeholder-neutral-content/40"
                       :placeholder="`Search ${settings?.website_name ?? 'Torrust'}`"
                       @keyup.enter="submitSearch"
                       @focusin="typingInSearch = true"
                       @focusout="typingInSearch = false"
                     >
                   </div>
-                  <div class="p-1 flex flex-col bg-neutral-content/10 font-medium text-neutral-content/50 rounded-lg">
+                  <div class="flex flex-col p-1 font-medium rounded-lg bg-neutral-content/10 text-neutral-content/50">
                     <template v-if="typingInSearch">
                       <span class="px-1 text-xs">enter</span>
                     </template>
@@ -118,12 +118,12 @@
             </div>
           </div>
         </div>
-        <ul class="menu menu-horizontal px-1">
+        <ul class="px-1 menu menu-horizontal">
           <li tabindex="0">
             <a>
               Explore
             </a>
-            <ul class="p-2 bg-base-100 shadow-2xl">
+            <ul class="p-2 shadow-2xl bg-base-100">
               <li>
                 <NuxtLink to="/torrents" replace>
                   Popular
@@ -132,9 +132,9 @@
             </ul>
           </li>
         </ul>
-        <div id="extra-options" class="flex flex-row flex-1 ml-auto items-center justify-end gap-3">
+        <div id="extra-options" class="flex flex-row items-center justify-end flex-1 gap-3 ml-auto">
           <template v-if="user?.username">
-            <NuxtLink to="/upload" class="btn btn-sm btn-primary inline-flex flex-nowrap justify-center items-center">
+            <NuxtLink to="/upload" class="inline-flex items-center justify-center btn btn-sm btn-primary flex-nowrap">
               <span class="flex flex-nowrap whitespace-nowrap">upload torrent</span>
             </NuxtLink>
             <div class="dropdown dropdown-end">
@@ -143,9 +143,9 @@
                   <UserCircleIcon />
                 </div>
               </label>
-              <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 drop-shadow-2xl bg-base-100 rounded-box w-52">
+              <ul tabindex="0" class="p-2 mt-3 menu menu-compact dropdown-content drop-shadow-2xl bg-base-100 rounded-box w-52">
                 <li v-if="user?.admin">
-                  <NuxtLink to="/admin/settings/general">
+                  <NuxtLink to="/admin/settings/backend">
                     Settings
                   </NuxtLink>
                 </li>
@@ -156,7 +156,7 @@
             </div>
           </template>
           <template v-else>
-            <button class="px-4 h-10 inline-flex flex-nowrap justify-center items-center bg-base-200 font-bold text-sm text-primary hover:bg-primary/20 capitalize rounded-xl cursor-pointer duration-500" @click="login()">
+            <button class="inline-flex items-center justify-center h-10 px-4 text-sm font-bold capitalize duration-500 cursor-pointer flex-nowrap bg-base-200 text-primary hover:bg-primary/20 rounded-xl" @click="login()">
               <span class="flex flex-nowrap whitespace-nowrap">login</span>
             </button>
           </template>
