f88ba256f24d408bac8db913e18854e1ef754f06
Jose Celano <josecelano@gmail.com>
2023-06-28 13:16:51 +0100

    feat!: change back default port to 3000
    We set the default port for the API to 3001, so we can use the deafult
    nuxt port here.

--- bin/install.sh
+++ bin/install.sh
@@ -2,7 +2,7 @@
 
 # Generate the .env file if it does not exist
 if ! [ -f "./.env" ]; then
-    echo "API_BASE_URL=http://localhost:3000/v1" > .env
+    echo "API_BASE_URL=http://localhost:3001/v1" > .env
 fi
 
 npm install
