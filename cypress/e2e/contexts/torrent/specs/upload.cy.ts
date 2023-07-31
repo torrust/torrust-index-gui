8b5f80f340535f0852288bfbefbeeb8540f6dc7b
Jose Celano <josecelano@gmail.com>
2023-07-31 14:03:54 +0100

    test: the random test generator endpoint accepts an UUID
    To identify the torrent, so that you do not need to parse resposne
    headers to get the filename or torrent identifier.

--- cypress/e2e/contexts/torrent/specs/upload.cy.ts
+++ cypress/e2e/contexts/torrent/specs/upload.cy.ts
@@ -1,27 +1,5 @@
+import { v4 as uuidv4 } from "uuid";
 import { RegistrationForm, random_user_registration_data } from "../../user/registration";
-
-// It extracts the filename from the content-disposition header.
-//
-// For example:
-//
-// From: attachment; filename=file-6dcf841b-bc75-4a2e-9aa0-8ca63b49df18.txt.torrent
-// It returns: file-6dcf841b-bc75-4a2e-9aa0-8ca63b49df18.txt.torrent
-function extractFilename (contentDisposition: string): string {
-  if (contentDisposition && contentDisposition.includes("attachment")) {
-    return contentDisposition.split(";")[1].split("=")[1].replace(/\"/g, "");
-  }
-  return "";
-}
-
-// It extracts the test torrent ID from the torrent file name.
-//
-// For example:
-//
-// From: file-6dcf841b-bc75-4a2e-9aa0-8ca63b49df18.txt.torrent
-// It returns: 6dcf841b-bc75-4a2e-9aa0-8ca63b49df18
-function extractTorrentIdFromFilename (filename: string): string {
-  return filename.split(".")[0].split("file-")[1];
-}
 
 describe("A registered user", () => {
   let registration_form: RegistrationForm;
@@ -36,44 +14,26 @@ describe("A registered user", () => {
   });
 
   it("should be able to upload a torrent", () => {
-    let torrentId = "";
+    let torrentId = uuidv4();
+    let torrentTitle = `title-${torrentId}`;
+    let torrentDescription = `title-${torrentId}`;
+    let torrentFilename = `file-${torrentId}.txt.torrent`;
+    let torrentPath = `cypress/fixtures/torrents/${torrentFilename}`;
 
-    cy.log("download a random torrent for the test");
+    cy.log("download a new test random torrent");
 
     cy.request({
-      url: "http://localhost:3001/v1/torrents/random",
+      url: `http://localhost:3001/v1/torrent/meta-info/random/${torrentId}`,
       encoding: "binary"
     }).then((response) => {
-      cy.log("random torrent downloaded");
-
-      const header = response.headers["content-disposition"];
-
-      let contentDisposition: string;
-      if (typeof header === "string") {
-        contentDisposition = header;
-      } else {
-        contentDisposition = header[0];
-      }
-
-      const filename = extractFilename(contentDisposition);
-      const torrentPath = `cypress/fixtures/torrents/${filename}`;
-      torrentId = extractTorrentIdFromFilename(filename);
-
-      // Alias the torrentId for later use
-      cy.wrap(torrentId).as("torrentId");
-
-      cy.log("torrent ID: ", torrentId);
-      cy.log("random torrent downloaded: ", torrentPath);
-
+      cy.log("random torrent downloaded to: ", torrentPath);
       cy.writeFile(torrentPath, response.body, "binary");
     });
 
     cy.visit("/upload");
 
-    cy.get("@torrentId").then((torrentId) => {
-      cy.get("input[data-cy=\"upload-form-title\"]").type(`title-${torrentId}`);
-      cy.get("textarea[data-cy=\"upload-form-description\"]").type(`description-${torrentId}`);
-    });
+    cy.get("input[data-cy=\"upload-form-title\"]").type(torrentTitle);
+    cy.get("textarea[data-cy=\"upload-form-description\"]").type(torrentDescription);
     cy.get("select[data-cy=\"upload-form-category\"]").select("software");
 
     // todo: add tag.
@@ -83,14 +43,12 @@ describe("A registered user", () => {
     // tests.
     // cy.get("input[data-cy=\"upload-form-tags\"]").select('fractals');
 
-    cy.get("@torrentId").then((torrentId) => {
-      cy.get("input[data-cy=\"upload-form-torrent-upload\"]").selectFile(
-        {
-          contents: `cypress/fixtures/torrents/file-${torrentId}.txt.torrent`,
-          fileName: `file-${torrentId}.torrent`,
-          mimeType: "application/x-bittorrent"
-        }, { force: true });
-    });
+    cy.get("input[data-cy=\"upload-form-torrent-upload\"]").selectFile(
+      {
+        contents: torrentPath,
+        fileName: torrentFilename,
+        mimeType: "application/x-bittorrent"
+      }, { force: true });
 
     cy.get("button[data-cy=\"upload-form-submit\"]").click();
 
