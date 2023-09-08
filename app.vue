6c727337ed8ec7f4e603c35d83e2d1d9be85c367
MMelchor <noreply>
2023-09-08 19:57:31 +0200

    refactor: [#247] search bar not working when in /torrents endpoint
    Code has been refactor and now looks cleaner and easier to understand.

--- app.vue
+++ app.vue
@@ -6,7 +6,7 @@
 
     <div class="flex flex-col w-full px-4 bg-base-300 md:px-8 lg:px-24">
       <div class="flex flex-col my-6" style="max-width: 100vw;">
-        <NuxtPage :page-key="route => route.fullPath" />
+        <NuxtPage />
       </div>
 
       <footer class="p-4 footer footer-center bg-base-300 text-base-content">
