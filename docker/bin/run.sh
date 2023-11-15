dd1167ecfc8e60406b2bb874b478b818c9486a10
Jose Celano <josecelano@gmail.com>
2023-11-15 11:09:15 +0000

    refactor: [#344] rename env vars
    Repo was renamed from `torrust-index-frontend` to `torrust-index-gui`.
    
    ```
    TORRUST_IDX_FRON_RUN_AS_USER       -> TORRUST_INDEX_GUI_RUN_AS_USER
    TORRUST_IDX_FRON_USER_UID          -> TORRUST_INDEX_GUI_USER_UID
    TORRUST_IDX_FRON_API_BASE_URL      -> TORRUST_INDEX_GUI_API_BASE_URL
    ```
    
    This commit renames anv vars to follow repo renaming. Besides, we are
    not using the abbreviation `IDX` for `INDEX` anymore.

--- docker/bin/run.sh
+++ docker/bin/run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-TORRUST_IDX_FRON_USER_UID=${TORRUST_IDX_FRON_USER_UID:-1001}
+TORRUST_INDEX_GUI_USER_UID=${TORRUST_INDEX_GUI_USER_UID:-1001}
 
 docker run -it \
-    --user="$TORRUST_IDX_FRON_USER_UID" \
+    --user="$TORRUST_INDEX_GUI_USER_UID" \
     --publish 3000:3000/tcp \
     torrust-index-frontend
