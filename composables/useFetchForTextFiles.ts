94a03b91ddae4c47459dd06ba09d9d84d4ee870b
Mario <mariomnt945@gmail.com>
2024-03-05 13:09:48 +0100

    feat: [#401] new composable for fetching files

---
+++ composables/useFetchForTextFiles.ts
@@ -0,0 +1,15 @@
+// Fetches a file from the server and
+// returns the content of the file as
+// text
+
+class FetchTextFileError extends Error {}
+
+export async function fetchTextFile (fileName: string) {
+  try {
+    const fetchResponse = await fetch(fileName);
+    const fileContent = await fetchResponse.text();
+    return fileContent;
+  } catch (error) {
+    return new FetchTextFileError(`Couldn't fetch the file: ${fileName}`);
+  }
+}
