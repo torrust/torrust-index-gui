3320ebd2843fe0313c711c4e03dce01b769224f5
Mario <mariomnt945@gmail.com>
2025-03-16 15:58:25 +0100

    refactor: [#698] changed styles and CSS

--- pages/admin/settings/users.vue
+++ pages/admin/settings/users.vue
@@ -1,27 +1,28 @@
 <template>
   <div class="flex flex-col max-w-md gap-2 mx-auto">
     <div class="flex flex-col gap-2">
-      <div class="pl-0.5 flex flex-wrap gap-2">
-        <input
-          v-model="searchQuery"
-          name="search"
-          type="text"
-          class="h-8 border-2 input input-bordered rounded-2xl placeholder-neutral-content"
-          :placeholder="`Filter by username`"
-        >
-        <div class="flex flex-wrap gap-1">
+      <div class="pl-0.5 flex flex-col gap-2">
+        <div class="flex gap-2">
           <TorrustSelect
             v-model:selected="selectedFilters"
-            class="ml-auto"
             :options="filteringOptions"
             label="Filter by"
           />
           <TorrustSelect
             v-model:selected="selectedSorting"
-            class="ml-auto"
             :options="sortingOptions"
             label="Sort by"
           />
+
+        </div>
+        <div class="flex flex-col w-full gap-2">
+          <input
+            v-model="searchQuery"
+            name="search"
+            type="text"
+            class="h-8 w-full border-2 input input-bordered rounded-2xl placeholder-neutral-content"
+            :placeholder="`Filter by username`"
+          >
         </div>
         <UserTable :user-profiles="userProfiles" />
         <Pagination
